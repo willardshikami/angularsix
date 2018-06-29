4bf65257eb2705a051575fd684c3f8b8192965e3
willardshikami <willardshikami@gmail.com>
2018-06-29 13:06:23 +0300

    updated register method

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const router = express.Router()
-const mongoose = require('mongoose')
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/user');
 
+//DB String
 const db = 'mongodb://shikami:shikami1@ds123946.mlab.com:23946/ng-auth';
 
+//DB connection
 mongoose.connect(db, err => {
   if(err){
     console.error('Error! ' + err)
@@ -12,9 +15,26 @@ mongoose.connect(db, err => {
   }
 })
 
+//init API
 router.get('/', (req, res) => {
   res.send('From the API');
 });
 
+
+//Request to register user
+//1.collect userdata from request object
+//2. convert it to model that mongoose understands... from ./
+//3. save the user to the db
+router.post('/register', (req, res) =>{
+  let userData = req.body;
+  let user = new User(userData);
+  user.save((error, registeredUser) => {
+    if(error){
+      console.log(error);
+    }else{
+      res.status(200).send(registeredUser);
+    }
+  })
+})
 
 module.exports = router;

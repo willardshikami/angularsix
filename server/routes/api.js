7641b926bd6d1fe76fa126cf5420bf6b679ecee5
willardshikami <willardshikami@gmail.com>
2018-07-01 11:25:14 +0300

    updated login API

--- server/routes/api.js
+++ server/routes/api.js
@@ -22,7 +22,7 @@ router.get('/', (req, res) => {
 
 
 //Request to register user
-//1.collect userdata from request object
+//1.collect/extract user information from request object
 //2.convert it to model that mongoose understands... from ./
 //3.save the user to the db
 router.post('/register', (req, res) =>{
@@ -38,6 +38,25 @@ router.post('/register', (req, res) =>{
 })
 
 //Request  to login user
-router.get()
+//Collect Userdata from req body
+//check if user exist in DB using findone function
+//verify the user password with the one in the DB
+//send status 200 if both information is true
+router.post('/login', (req, res) => {
+  let userData = req.body;
+
+  User.findOne({email: userData.email}, (error, user) => {
+    if(error){
+      console.log(error);
+    }else
+      if(!user){
+        res.status(401).send('Invalid email')
+      }else
+      if(user.password !== userData.password){
+        res.status(401).send('Invalid password')
+      }else
+      res.status(200).send(user);
+  })
+})
 
 module.exports = router;
